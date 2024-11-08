a6434b07225b627a00247c16b6ce7f503922a6e0
reuben-raj <reuben.raj90@gmail.com>
2024-11-08 21:59:42 +0800

    first commit

---
+++ src/main/java/com/e3/api/product_device/ProductDeviceApplication.java
@@ -0,0 +1,13 @@
+package com.e3.api.product_device;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDeviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e3/api/product_device/ProductDeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e3.api.product_device;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
